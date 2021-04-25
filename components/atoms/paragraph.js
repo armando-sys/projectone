96c1a98bc654fc5c569daf2f09ca7149ca68836a
armando-sys <patrickarmando87@gmail.com>
2021-04-25 22:20:48 +0700

    Update Last

--- components/atoms/paragraph.js
+++ components/atoms/paragraph.js
@@ -2,19 +2,19 @@
 export default function AtomText({value,size,fontFamily,color,margin,customClass,padding,weight,width}){
     return (
     <>
-    <p 
-    className={customClass}
-    style={{
-    color:color,
-    fontSize:size,
-    fontFamily:fontFamily,
-    margin:margin,
-    padding:padding,
-    weight:weight,
-    width:width,
-    }}>
-        {value}
-    </p>
+        <p 
+        className={customClass}
+        style={{
+        color:color,
+        fontSize:size,
+        fontFamily:fontFamily,
+        margin:margin,
+        padding:padding,
+        weight:weight,
+        width:width,
+        }}>
+            {value}
+        </p>
     </>
     );
 }
--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,4 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AtomText from "../components/atoms/paragraph";
 
 export default function test (){
@@ -6,24 +6,42 @@ export default function test (){
     <>
     <center>
     
-            <div className="row container">
+            <div className="container mt-4">
+                <div className="templateBox row">
 
-                <div className="col-md-6">
-                    <AtomText
-                    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
-                    fontFamily = "Roboto" 
-                    size="12px" 
-                    />;
-                </div>
-                <div className="col-md-6">
-                    <AtomText
-                    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
-                    fontFamily = "Roboto" 
-                    size="12px" 
-                    />;
-                </div>
+                    <div className="col-6 text-left">
 
+                        <AtomText
+                        value = "Next.JS Framework"
+                        fontFamily = "Roboto" 
+                        size="20px"
+                        margin="10px 0px 0px 0px" 
+                        />  
+
+                        <AtomText
+                        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+                        fontFamily = "Roboto" 
+                        size="12px" 
+                        />
+                    </div>
+                    <div className="col-6 text-left">
+                        <AtomText
+                        value = "Framework Laravel"
+                        fontFamily = "Roboto" 
+                        size="20px"
+                        margin="10px 0px 0px 0px"   
+                        />
+
+                        <AtomText
+                        value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+                        fontFamily = "Roboto" 
+                        size="12px" 
+                        />
+                    </div>
+
+                </div>
             </div>
+
     </center>
 
     </>
