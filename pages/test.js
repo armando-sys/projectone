2ac59a051df9217f7412d14ed19362de1901e81e
armando-sys <patrickarmando87@gmail.com>
2021-04-25 22:00:53 +0700

    Update test.js

--- pages/test.js
+++ pages/test.js
@@ -2,18 +2,30 @@
 import AtomText from "../components/atoms/paragraph";
 
 export default function test (){
-    return (
+    return(
     <>
-    <div>
+    <center>
+    
+            <div className="row container">
 
-    <AtomText
-    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
-    fontFamily = "Roboto" 
-    size="12px" 
-    />;
+                <div className="col-md-6">
+                    <AtomText
+                    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+                    fontFamily = "Roboto" 
+                    size="12px" 
+                    />;
+                </div>
+                <div className="col-md-6">
+                    <AtomText
+                    value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+                    fontFamily = "Roboto" 
+                    size="12px" 
+                    />;
+                </div>
 
-    </div>   
+            </div>
+    </center>
+
     </>
     )
-
 }
