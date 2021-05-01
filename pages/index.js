f785977773cd73782ebb6844295ca4a7f413e0c0
armando-sys <patrickarmando87@gmail.com>
2021-05-01 22:05:52 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,125 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import AtomText from "../components/atoms/paragraph";
+import AtomButton from "../components/atoms/button";
 
-export default function Home() {
+export default function test() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Navbar bg="primary" variant="light">
+        <Navbar.Brand href="#home">
+          <img src="/homepage/nav_logo.png"
+            style={{
+              width: "50px",
+              margin: "0 0 0 100px",
+              filter: "drop-shadow(1px 1px 1px #666666)",
+            }} />
+        </Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link className="text-light" href="#home">Trend</Nav.Link>
+          <Nav.Link className="text-light" href="#features">Explore</Nav.Link>
+          <Nav.Link className="text-light" href="#pricing">Collection</Nav.Link>
+          <Nav.Link className="text-light" href="#pricing">About Us</Nav.Link>
+        </Nav>
+        <Form inline>
+          <img src="/homepage/cart.png" style={{ marginRight: "30px" }} />
+          <img src="/homepage/user.png" style={{ marginRight: "100px" }} />
+        </Form>
+      </Navbar>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+      <div className="container mt-4">
+        <div className="templateBox row">
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className="col-6 text-left">
+            <AtomText
+              value="Next.JS Framework"
+              fontFamily="Roboto"
+              size="20px"
+              padding="10px"
+              margin="0 0 0 0"
+            />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+            <AtomText
+              value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+              fontFamily="Roboto"
+              size="12px"
+              align="justify"
+              padding="10px"
+              margin="0px"
+            />
+          </div>
+          <div className="col-6 text-left">
+            <AtomText
+              value="Framework Laravel"
+              fontFamily="Roboto"
+              size="20px"
+              padding="10px"
+              margin="0 0 0 0"
+            />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <AtomText
+              value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tristique nisl vel tellus fringilla, id hendrerit ipsum faucibus. Sed id nunc id risus rutrum dignissim ac sed purus. Nunc vestibulum mauris eleifend luctus tincidunt. Etiam velit leo, vehicula non massa eget, laoreet auctor neque. Suspendisse accumsan sed nisl ac congue. Duis a neque venenatis, ultrices quam eu, tincidunt eros. Proin imperdiet tincidunt nisl ut posuere. Etiam mattis pharetra mauris, eu tempor sem elementum vel. Proin ac dolor efficitur, varius lorem in, porttitor nunc."
+              fontFamily="Roboto"
+              size="12px"
+              align="justify"
+              padding="10px"
+              margin="0px"
+            />
+          </div>
+          <div className="col-12">
+            <AtomText value="Patrick Armando"
+              size="24px"
+              variant="small-caps"
+              family="Arial, Helvetica, sans-serif"
+              weight="700"
+              padding="10px"
+              margin="-10px 0 0 0"
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            />
+            <AtomText value="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla sem tortor, ut gravida velit efficitur in. Phasellus finibus, turpis eu suscipit varius, quam erat suscipit sapien, eget ultricies mi nunc id nunc. 
+                        Quisque varius eu turpis vel egestas. Pellentesque placerat purus felis, id ornare tortor vestibulum ut. Ut aliquam bibendum volutpat. In accumsan dictum pellentesque. 
+                        Cras ultrices scelerisque nibh vel rhoncus. Etiam nec magna vitae felis hendrerit porttitor ut pretium orci. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce fringilla sem tortor, ut gravida velit efficitur in. 
+                        Phasellus finibus, turpis eu suscipit varius, quam erat suscipit sapien, eget ultricies mi nunc id nunc. 
+                        Quisque varius eu turpis vel egestas. Pellentesque placerat purus felis, id ornare tortor vestibulum ut. 
+                        Ut aliquam bibendum volutpat. In accumsan dictum pellentesque. Cras ultrices scelerisque nibh vel rhoncus. 
+                        Etiam nec magna vitae felis hendrerit porttitor ut pretium orci."
+              size="12px"
+              padding="0px"
+              align="justify"
+              margin="0px"
+              padding="10px 10px 10px 10px"
+              shadow="0 4px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 20px 0 rgba(0, 0, 0, 0.15)"
+              fontFamily="Roboto"
+
+            />
+            <AtomButton value="Read More"
+              background="blue"
+              border="1px solid"
+              padding="10px 20px"
+              margin="10px 0 20px 10px"
+              color="white"
+              radius="10px"
+              float="right"
+              variant="outline-light"
+
+
+            />
+            <AtomButton
+              value="Learn More"
+              background="Green"
+              border="none"
+              padding="10px 20px"
+              margin="10px 0 20px 20px"
+              color="white"
+              radius="10px"
+              float="right"
+            />
+
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+      </div>
+    </>
   )
 }
