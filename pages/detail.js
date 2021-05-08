477e22a782c9f9deb996d151d09d15a094241816
armando-sys <patrickarmando87@gmail.com>
2021-05-08 22:33:15 +0900

    updateDetailProductBag

--- pages/detail.js
+++ pages/detail.js
@@ -12,47 +12,115 @@ export default function detail() {
             <div className="row container mt-5">
                 <div className="col-6">
                     <div className="row container">
-                        <div className="col-4" style={{border:"1px solid red"}}>
-                            <AtomImage background="#dedede"/>Lorem Ipsum
-
+                        <div className="col-4">
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginTop: "10px",
+                                    marginBottom: "20px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }} />
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginBottom: "20px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }} />
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginBottom: "20px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }}
+                            />
                         </div>
-                        <div className="col-8" style={{border:"1px solid blue"}}>
-                        <AtomImage
-                                background-image="url('../public/detail/bag.png')"
-                                height="500px"
-                                size="contain"
-                                repeat="no-repeat"
-                                position="center"
-                        />
 
+                        <div className="col-8 templateDetailProduct" style={{ height: "370px" }}>
+                            <img src="/detail/bag.png" />
                         </div>
                     </div>
+                    <div className="row mt-3">
+                        <div className="col-4" />
+
+                        <div className="col-8">
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginRight: "25px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }} />
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginRight: "25px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }} />
+                            <img src="/detail/bag.png"
+                                style=
+                                {{
+                                    width: "90px",
+                                    padding: "5px",
+                                    marginRight: "20px",
+                                    border: "1px solid",
+                                    background: "#FFFFFF",
+                                    border: "1px solid #DEDEDE",
+                                    borderRadius: "10px",
+                                }}
+                            />
+                        </div>
+
+                    </div>
                 </div>
+
                 <div className="col-6 text-left">
                     <AtomText value="Hamlin Cellyn Tas Ransel Wanita Japan Style Big Compartment"
                         size="36px"
                         weight="700"
                     />
                     <AtomText value="Stock : 50"
-                    size="14px" color="#9D9D9D"
+                        size="14px" color="#9D9D9D"
                     />
                     <AtomText value="$50.00"
-                    size="30px"
+                        size="30px"
                     />
                     <AtomText value="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-                    size="14px" align="justify"
-                    />     
+                        size="14px" align="justify"
+                    />
                     <AtomButton value="Add to Cart"
-                    radius="5px" 
-                    background="#0086CF"
-                    color="#FFFFFF"
-                    border="none"
-                    padding="10px 20px 10px 20px"
-                    margin="75px 0 0 0"
+                        radius="10px"
+                        background="#0086CF"
+                        color="#FFFFFF"
+                        border="none"
+                        padding="10px 40px 10px 10px"
+                        margin="45px 0 40px 0"
                     />
                 </div>
             </div>
         </center>
-    </>)
-        ;
+    </>);
 }
